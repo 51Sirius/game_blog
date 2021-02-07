a1c48250bd2a7772fb6357abe760831b7152f499
51Sirius <71227522+51Sirius@users.noreply.github.com>
2021-02-07 18:54:38 +0300

    correct homepage

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+articles = {
+    1: {},
+    2: {},
+}
+
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def homepage():
+    return render_template('index.html', articles=articles)
 
 
 if __name__ == '__main__':
