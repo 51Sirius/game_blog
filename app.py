02a07c1de8c15b5e273d8e8710cce7e9c7277310
51Sirius <71227522+51Sirius@users.noreply.github.com>
2021-02-08 15:11:28 +0300

    Correct categories

--- app.py
+++ app.py
@@ -12,18 +12,18 @@ articles = {
         'date': '09.02.2021'},
 }
 
-categories = {
-    1: {'name': 'Lost Ark'},
-    2: {'name': 'GTA'},
-    3: {'name': 'Game Dev'},
-    4: {'name': 'Steam'},
-    5: {'name': 'Company'}
-}
+categories = [
+    {'name': 'Lost Ark', 'id': 1},
+    {'name': 'GTA', 'id': 2},
+    {'name': 'Game Dev', 'id': 3},
+    {'name': 'Steam', 'id': 4},
+    {'name': 'Company', 'id': 5}
+]
 
 
 @app.route('/')
 def homepage():
-    return render_template('index.html', articles=articles)
+    return render_template('index.html', articles=articles, categories=categories)
 
 
 if __name__ == '__main__':
