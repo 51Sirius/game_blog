26b858b0b628b19d910046d9c52459b69f648ffb
51Sirius <71227522+51Sirius@users.noreply.github.com>
2021-02-11 19:10:23 +0300

    create categories.py and article.py

--- app.py
+++ app.py
@@ -1,35 +1,8 @@
 from flask import Flask, render_template
+from categories import categories
+from article import articles
 
 app = Flask(__name__)
-
-articles = [
-    {'title': 'Lost ark',
-     'text': 'Благодарим вас за интерес и вопросы, которые вы оставляли к публикациям с анонсом плана обновлений и презентации LOA ON. Команда локализации передала разработчикам комментарии русскоязычного сообщества, и мы вместе продолжим радовать вас подробной информацией об игре и ее перспективах. Если вы хотите узнать больше о будущем LOST ARK, оставляйте свои вопросы в специальной форме. Мы выберем самые интересные и часто встречающиеся, а ответит на них в видеоформате директор разработки Smilegate RPG — Geum Kang-seon.',
-     'date': '08.02.2021',
-     'big': True,
-     'id': 1,
-     'img': True,
-     'img_id': 'https://im0-tub-ru.yandex.net/i?id=83291a42738314f71c7455101901b2ac&n=13',
-     'categories': [1]
-     },
-    {'title': 'Gaming IT',
-     'text': 'В следующем месяце Disco Elysium дебютирует на консолях и получит расширенную версию — The Final Cut. Среди нововведений — политические квесты, о которых во время анонса нового издания разработчики почти ничего не говорили. Зато поговорили в интервью Push Square.',
-     'date': '09.02.2021',
-     'big': False,
-     'id': 2,
-     'img': False,
-     'img_id': '',
-     'categories': [5]
-     },
-]
-
-categories = [
-    {'name': 'Lost Ark', 'id': 1},
-    {'name': 'GTA', 'id': 2},
-    {'name': 'Game Dev', 'id': 3},
-    {'name': 'Steam', 'id': 4},
-    {'name': 'Company', 'id': 5}
-]
 
 
 @app.route('/')
---
+++ categories.py
@@ -0,0 +1,7 @@
+categories = [
+    {'name': 'Lost Ark', 'id': 1},
+    {'name': 'GTA', 'id': 2},
+    {'name': 'Game Dev', 'id': 3},
+    {'name': 'Steam', 'id': 4},
+    {'name': 'Company', 'id': 5}
+]
