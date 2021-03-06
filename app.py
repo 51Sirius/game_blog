50312d1b0c19f9f9b769f0b65cef9a3f02bba68b
51Sirius <71227522+51sirius@users.noreply.github.com>
2021-03-06 21:33:42 +0300

    Add sql users

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
 from categories import categories
 from article import articles, find_by_text
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
 
 @app.route('/')
---
+++ datebase.py
@@ -0,0 +1,8 @@
+from app import db
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(70), nullable=True)
+    mail = db.Column(db.String(70), nullable=True, unique=True)
+    password = db.Column(db.String(70), nullable=True)
