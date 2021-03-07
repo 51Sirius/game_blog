018d51ae4c2dbae206a796123c2e833275cdbd88
51Sirius <71227522+51sirius@users.noreply.github.com>
2021-03-07 13:14:56 +0300

    Add article database

--- database.py
+++ database.py
@@ -8,3 +8,17 @@ class Users(db.Model):
     name = db.Column(db.String(70), nullable=True)
     mail = db.Column(db.String(70), nullable=True, unique=True)
     password = db.Column(db.String(70), nullable=True)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(70), nullable=True)
+    body = db.Column(db.String(70), nullable=True, unique=True)
+    date = db.Column(db.String(70), nullable=True)
+    big = db.Column(db.Boolean, default=True)
+    img = db.Column(db.Boolean, default=False)
+    img_url = db.Column(db.String(70))
+
+
+class Categories(db.Model):
+    pass
