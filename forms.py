8ad2063112d4e3fda6d5aa8e0b3b53012b66f418
51Sirius <71227522+51sirius@users.noreply.github.com>
2021-03-09 23:31:59 +0300

    Add to database example article, category and user

--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
+from wtforms.widgets import TextArea
 
 
 class ArticleForm(FlaskForm):
-    title = StringField('Название статьи', validators=[DataRequired()])
-    body = StringField('Текст статьи', validators=[DataRequired()])
+    title = StringField('Название статьи', validators=[DataRequired()], widget=TextArea())
+    body = StringField('Текст статьи', validators=[DataRequired()], widget=TextArea())
     category_id = IntegerField('ID Категории', validators=[DataRequired()])
     author_id = IntegerField('ID Автора', validators=[DataRequired()])
