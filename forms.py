30612a90dab3035b184ee2ef8225064fbf54c4e8
51Sirius <71227522+51sirius@users.noreply.github.com>
2021-03-12 19:38:01 +0300

    Update forms

--- forms.py
+++ forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, BooleanField
 from wtforms.validators import DataRequired
-from wtforms.widgets import TextArea
+from wtforms.widgets import TextArea, TextInput
 
 
 class ArticleForm(FlaskForm):
-    title = StringField('Название статьи', validators=[DataRequired()], widget=TextArea())
+    title = StringField('Название статьи', validators=[DataRequired()], widget=TextInput())
     body = StringField('Текст статьи', validators=[DataRequired()], widget=TextArea())
-    category_id = IntegerField('ID Категории', validators=[DataRequired()])
-    author_id = IntegerField('ID Автора', validators=[DataRequired()])
+    category_id = IntegerField('ID Категории', validators=[DataRequired()], widget=TextInput('number'))
+    author_id = IntegerField('ID Автора', validators=[DataRequired()], widget=TextInput('number'))
+    image = StringField('Ссылка на картинку', validators=[DataRequired()], widget=TextInput())
