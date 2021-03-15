afe22344e73422d7fe3613e25280cccfb3e80119
51Sirius <71227522+51sirius@users.noreply.github.com>
2021-03-15 18:20:25 +0300

    Create forms for login

--- forms.py
+++ forms.py
@@ -16,3 +16,8 @@ class Registration(FlaskForm):
     nickname = StringField('Никнэйм', validators=[DataRequired()], widget=TextInput())
     mail = StringField('Почта', validators=[DataRequired()], widget=TextInput())
     password = StringField('Пароль', validators=[DataRequired()], widget=PasswordInput())
+
+
+class Login(FlaskForm):
+    mail_or_name = StringField('Ник или почта', validators=[DataRequired()], widget=TextInput())
+    password = StringField('Пароль', validators=[DataRequired()], widget=PasswordInput())
