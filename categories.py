b95ef3d28a121ae6111dc739f7d5ac5133b4fa6d
51Sirius <71227522+51sirius@users.noreply.github.com>
2021-03-16 19:22:48 +0300

    Delete category

--- categories.py
+++
@@ -1,7 +0,0 @@
-categories = [
-    {'name': 'Lost Ark', 'id': 1},
-    {'name': 'GTA', 'id': 2},
-    {'name': 'Game Dev', 'id': 3},
-    {'name': 'Steam', 'id': 4},
-    {'name': 'Company', 'id': 5}
-]
